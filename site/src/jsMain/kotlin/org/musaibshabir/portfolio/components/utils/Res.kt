2c8189906806ddc7b2cc9ac9c0ef937e58ceb509
Musaib Shabir <bnny633@gmail.com>
2025-03-20 16:14:40 +0530

    Utils Added

---
+++ site/src/jsMain/kotlin/org/musaibshabir/portfolio/components/utils/Res.kt
@@ -0,0 +1,118 @@
+package org.musaibshabir.website.utils
+
+import com.varabyte.kobweb.compose.ui.graphics.Color
+
+object Res {
+
+    object Constants {
+        const val RESUME_URL = "https://drive.google.com/file/d/11M1RPYv3xNdwc88zEVvHVPVSrb5G9Fna/view?usp=drive_link"
+        const val LINKEDIN_URL = "https://www.linkedin.com/in/binayshaw7777/"
+        const val GITHUB_URL = "https://github.com/binayshaw7777"
+        const val TWITTER_URL = "https://twitter.com/binayplays7777"
+        const val MEDIUM_URL = "https://medium.com/@binayshaw7777"
+        const val MAIL_TO = "mailto:binayshaw7777@gmail.com"
+        const val PROJECT_READBUD_URL = "https://github.com/binayshaw7777/ReadBud"
+
+
+        const val GREETING = "Hello I'm"
+        const val NAME = "Musaib Shabir"
+        const val SUB_HEADLINE = "A Professional Kotlin & Android Developer"
+        const val RESUME = "Resume"
+        const val MY_WORK = "My Work"
+
+        const val ABOUT_ME_TITLE = "About me"
+        const val ABOUT_ME_TEXT = "I’m a Computer Science Engineering graduate with a strong passion for technology, problem-solving, and innovation. " +
+                "I specialize in Modern Android development using Kotlin, & Jetpack Compose. " +
+                "My interests extend to Deep learning, and Artificial Intelligence. Beyond coding, " +
+                "I’m fascinated by Cosmos and always eager to learn more about the universe, its mysteries, and the science behind it. " +
+                "Constantly exploring new ideas and building solutions that make an impact \uD83D\uDE0A"
+
+
+        const val EXPERIENCE_TITLE = "Work Experience."
+        const val EXPERIENCE_SECTION_TEXT = "I’m a Software Developer Engineer, specializing in Android & Backend Development with "
+        const val FIVE_MONTH = "5 months"
+        const val OF_INTERNSHIP = " of Internship Experience."
+
+        const val SKILLS_AND_TOOLS_TITLE = "Skills & Tools."
+
+        const val PHOTOGRAPH_TITLE = "Sometimes I love to click."
+
+        const val PROJECT_TITLE = "My Side Projects."
+        const val PROJECT_SECTION_TEXT = "I really have a bad habit of creating projects over projects XD"
+        const val PROJECT_REPOSITORIES_URL = "https://github.com/binayshaw7777?tab=repositories"
+        const val MORE_ON_GITHUB = "More on GitHub."
+    }
+
+    object Images {
+        const val BACKGROUND_CIRCLES = "images/background_circles.svg"
+        const val PORTAL_STAR = "images/portal_star.svg"
+        const val NAVIGATION_ARROW = "images/Navigation_Arrow.svg"
+        const val CUSTOM_CURSOR_DARK = "images/Custom_Cursor_Dark.svg"
+        const val CUSTOM_CURSOR_LIGHT = "images/Custom_Cursor_Light.svg"
+
+
+
+
+        // Skills
+        const val ANDROID_STUDIO_LIGHT_LOGO = "images/Skills/android_studio_light.svg"
+        const val ANDROID_STUDIO_DARK_LOGO = "images/Skills/android_studio_dark.svg"
+        const val INTELLIJ_IDEA_LOGO = "images/Skills/intellij_idea.svg"
+        const val KOTLIN_LOGO = "images/Skills/kotlin.svg"
+        const val COMPOSE_LOGO = "images/Skills/compose.svg"
+        const val JAVA_LOGO = "images/Skills/java.svg"
+        const val CPP_LOGO = "images/Skills/cpp.svg"
+        const val JETPACK_LOGO = "images/Skills/jetpack.svg"
+        const val PYTHON_LOGO = "images/Skills/python.svg"
+        const val KTOR_LOGO = "images/Skills/ktor.svg"
+        const val FIREBASE_LOGO = "images/Skills/firebase.svg"
+        const val MARKDOWN_LOGO = "images/Skills/Markdown.svg"
+        const val ANDROID_LOGO = "images/Tools/Android.svg"
+        const val FIGMA_LOGO = "images/Tools/figma.svg"
+        const val GIT_LOGO = "images/Tools/Git.svg"
+        const val GITHUB_DARK_LOGO = "images/Tools/github_dark.svg"
+        const val GITHUB_LIGHT_LOGO = "images/Tools/github_light.svg"
+
+
+        // Socials
+        const val BEHANCE = "images/Socials/behance.svg"
+        const val GITHUB_FILLED = "images/Socials/github-filled.svg"
+        const val LINKEDIN = "images/Socials/linkedin.svg"
+        const val MEDIUM = "images/Socials/medium.svg"
+        const val TWITTER_X = "images/Socials/twitter-x.svg"
+
+        // Photographs
+        const val IMAGE_1 = "images/Photographs/Image 1.png"
+        const val IMAGE_2 = "images/Photographs/Image 2.png"
+        const val IMAGE_3 = "images/Photographs/Image 3.png"
+        const val IMAGE_4 = "images/Photographs/Image 4.png"
+        const val IMAGE_5 = "images/Photographs/Image 5.png"
+        const val IMAGE_6 = "images/Photographs/Image 6.png"
+        const val IMAGE_7 = "images/Photographs/Image 7.png"
+        const val IMAGE_8 = "images/Photographs/Image 8.png"
+        const val IMAGE_9 = "images/Photographs/Image 9.png"
+        const val IMAGE_10 = "images/Photographs/Image 10.png"
+        const val IMAGE_11 = "images/Photographs/Image 11.png"
+        const val IMAGE_12 = "images/Photographs/Image 12.png"
+
+        // Projects
+        const val PROJECT_JUSTAP = "images/Projects/Project_JusTap.png"
+        const val PROJECT_MEDIFY = "images/Projects/Project_Medify.png"
+        const val PROJECT_PASSGENIE = "images/Projects/Project_PassGenie.png"
+        const val PROJECT_PRESIN = "images/Projects/Project_PresIN.png"
+        const val PROJECT_READBUD = "images/Projects/Project_ReadBud.png"
+    }
+
+    object Fonts {
+        const val Space_Grotesk = "Space Grotesk"
+        const val Tauri = "Tauri"
+        const val DM_SANS = "DM Sans"
+    }
+
+    object Colors {
+        val FOOTER_COLOR_LIGHT = Color.Companion.rgb(0xF1F1F1)
+        val FOOTER_COLOR_DARK = Color.Companion.rgb(0x0D0D0D)
+        val GLASS_BOX_BORDER_COLOR_LIGHT = Color.Companion.rgb(0xE9E9E9)
+        val GLASS_BOX_BORDER_COLOR_DARK = Color.Companion.rgb(0x4E4E4E)
+        val DARK_GRAY_TEXT_COLOR = Color.Companion.rgb(0x7B7B7B)
+    }
+}
